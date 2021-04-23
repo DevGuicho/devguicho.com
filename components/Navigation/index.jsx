540ae3b936124e6f491b59622c9bae9ae0f53f0f
Luis Vazquez <luvazpa@gmail.com>
2021-04-23 11:32:13 -0500

    Navbar effects added and header fixed at top

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import styles from './Navigation.module.scss'
 
 const Navigation = () => {
+  const [isChecked, setIsChecked] = useState(false)
+  const handleChangle = (e) => {
+    console.log(e.target.checked)
+    setIsChecked(e.target.checked)
+  }
   return (
     <nav className={styles.Navigation}>
-      <span className={styles.Bar}>
+      <label className={styles.toggle} htmlFor='bar'>
         <i className={styles.facebook}></i>
-      </span>
-      <ul>
-        <li>
+      </label>
+      <input
+        className={styles.check}
+        type='checkbox'
+        name='bar'
+        id='bar'
+        checked={isChecked}
+        onChange={handleChangle}
+      />
+      <ul className={styles.list}>
+        <li onClick={() => setIsChecked(false)}>
           <a href='#about'>About me</a>
         </li>
-        <li>
+        <li onClick={() => setIsChecked(false)}>
           <a href='#projects'>Proyectos</a>
         </li>
-        <li>
+        <li onClick={() => setIsChecked(false)}>
           <Link href='/blog'>
             <a>Blog</a>
           </Link>
